69d386bb08d9eb88298ad061b1722b0f802f210a
itsdev04 <imkrdev@gmail.com>
2026-05-10 22:03:00 +0530

    first commit

---
+++ src/main/java/com/devworks/GithubActionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devworks/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.devworks.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TestController {
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome(){
+        return new ResponseEntity<>("Welcome to the Github Actions demo application!", HttpStatus.OK);
+    }
+}
